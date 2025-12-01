f77a843cb4051dc2a6babccd48c75028232fecc7
CleverSalvador <cleversalvador97@gmail.com>
2025-12-01 08:50:26 -0500

    feat: first project for graphql

---
+++ graphql/src/main/java/com/course/graphql/config/DataFakerConfig.java
@@ -0,0 +1,14 @@
+package com.course.graphql.config;
+
+import net.datafaker.Faker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataFakerConfig {
+
+    @Bean
+    Faker faker() {
+        return new Faker();
+    }
+}
---
+++ graphql/src/main/java/com/course/graphql/model/Hello.java
@@ -0,0 +1,21 @@
+package com.course.graphql.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Hello {
+
+    @NotNull
+    private String text;
+
+    private Integer randomNumber;
+}
